33caa1d45d580667ab64c7e6173cf19b9ee1459e
Julien De Reuse <julien.de.reuse@student.ehb.be>
2023-03-22 16:40:31 +0100

    to be conti.

--- js/index.js
+++ js/index.js
@@ -1,15 +1,31 @@
 "use strict";
 
-const app = {
-    items: [],
-    init() {
-    },
-    getData() {
-    },
-    onSearch() {
-    },
-    render(message) {
-    }
+import Jokes from "../js/Jokes.js";
 
-}
-app.init()
+const app = {
+	items: [],
+	init() {
+		this.getData();
+	},
+	getData() {
+		fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,racist,sexist,explicit&amount=10")
+			.then(function (response) {
+				console.log(response);
+				return response.json();
+			})
+			.then(function (json) {
+				json.jokes.forEach(function (jokesData) {
+					console.log(Jokes);
+					const joke = new Jokes(jokesData.category, jokesData.setup, jokesData.delivery);
+					app.items.push(joke);
+					app.render(joke);
+				});
+			});
+	},
+	onSearch() {},
+	render(message) {
+		console.log(app.items);
+		document.querySelector("#list").insertAdjacentHTML("beforeend", message.htmlString);
+	},
+};
+app.init();
